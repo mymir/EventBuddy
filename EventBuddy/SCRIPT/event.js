86719f93ea94dee5fc5e7ffdefe8adf6eb20d55f
mnhurd <mnhurd@ncsu.edu>
2018-04-21 11:27:14 -0400

    Added a constructor to the user and event classes

---
+++ EventBuddy/SCRIPT/event.js
@@ -0,0 +1,16 @@
+class Event() {
+   constructor() {
+      this.name;         //the name of the event
+      this.location;     //location object
+      this.attendees;    //array list of attendees (profile objects)
+      this.picture;      //string of link to picture
+      this.description;  //description
+      this.label;        //The organizer/type of the activity
+      this.tag;          //array of tags
+   }
+
+   //fnName() {
+   // fnbody
+   //}
+
+}
---
+++ EventBuddy/SCRIPT/user.js
@@ -0,0 +1,10 @@
+class User() {
+   constructor() {
+      this.name;         //the name of the user
+      this.friends;      //the friends of this user -> array of users
+      this.picture;      //string of link to picture
+      this.bio;          //description of the user by the user
+      this.attending;    //array of events the user is attending
+      this.planning;     //events the user wants to attend
+   }
+}
